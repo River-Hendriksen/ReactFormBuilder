d6e2f1caf97b7835d1e1fe91875d4f49c01cfb12
River Hendriksen <RIH27@pitt.edu>
2023-04-07 13:20:16 -0400

    working rollup

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,13 +1,21 @@
 import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
+import css from "rollup-plugin-import-css";
+import commonJs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import terser from "@rollup/plugin-terser";
 import packageJson from "./package.json" assert { type: "json" };
 
 export default [
   {
     input: "src/index.ts",
+    external: [...Object.keys(packageJson.peerDependencies || {})],
     output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
       {
         file: packageJson.module,
         format: "esm",
@@ -15,9 +23,11 @@ export default [
       },
     ],
     plugins: [
-      resolve(),
-      commonjs(),
+      css(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      commonJs(),
+      resolve(),
+      terser(),
     ],
   },
   {
--- src/components/yupSchemaCreator/yupSchemaGenerator.tsx
+++ src/components/yupSchemaCreator/yupSchemaGenerator.tsx
@@ -1,130 +1,141 @@
-import * as yup from 'yup';
-import { validationRequirementProps, whenValidationProps, yupFormBuilderProps } from '../../interfaces/yupSchemaInterfaces';
+import * as yup from "yup";
+import {
+  validationRequirementProps,
+  whenValidationProps,
+  yupFormBuilderProps,
+} from "../../interfaces/yupSchemaInterfaces";
 
-export const schema = {
-    type: 'object',
-    properties: {
-        numberRange: {
-            type: 'number',
-            validations: [
-                {
-                    type: 'min',
-                    params: [0],
-                },
-                {
-                    type: 'max',
-                    params: [10],
-                },
-                {
-                    type: 'transform',
-                    params: ['(_, val) => (val ? Number(val) : null)'],
-                },
-            ],
-        },
-        name: {
-            type: 'number',
-            validations: [
-                {
-                    type: 'when',
-                    params: [
-                        {
-                            comparatorVariable: 'isBig',
-                            is: true,
-                            then: [
-                                {
-                                    type: 'min',
-                                    params: [5],
-                                },
-                            ],
-                            otherwise: [
-                                {
-                                    type: 'min',
-                                    params: [0],
-                                },
-                            ],
-                        },
-                    ],
-                },
-            ],
-        },
-        test: {
-            type: 'string',
-            validations: ['required'],
-        },
-    },
-};
+// export const schema = {
+//   type: "object",
+//   properties: {
+//     numberRange: {
+//       type: "number",
+//       validations: [
+//         {
+//           type: "min",
+//           params: [0],
+//         },
+//         {
+//           type: "max",
+//           params: [10],
+//         },
+//         {
+//           type: "transform",
+//           params: ["(_, val) => (val ? Number(val) : null)"],
+//         },
+//       ],
+//     },
+//     name: {
+//       type: "number",
+//       validations: [
+//         {
+//           type: "when",
+//           params: [
+//             {
+//               comparatorVariable: "isBig",
+//               is: true,
+//               then: [
+//                 {
+//                   type: "min",
+//                   params: [5],
+//                 },
+//               ],
+//               otherwise: [
+//                 {
+//                   type: "min",
+//                   params: [0],
+//                 },
+//               ],
+//             },
+//           ],
+//         },
+//       ],
+//     },
+//     test: {
+//       type: "string",
+//       validations: ["required"],
+//     },
+//   },
+// };
 
 const yupArgCreator = (
-    validation: validationRequirementProps | string,
-    validator: any
+  validation: validationRequirementProps | string,
+  validator: any
 ) => {
-    //if it's a string, it's a function in the yup schema
-    if (typeof validation === 'string') {
-        return validator[validation as keyof typeof validator]();
+  // if validation is a string, return the validator with that string as a key
+  if (typeof validation === "string") {
+    return validator[validation as keyof typeof validator]();
+  }
+
+  // if validation is an object, destructure type and params
+  const { params, type } = validation;
+
+  // if the validator does not have the key in the type, return
+  if (!validator[type as keyof typeof validator]) {
+    return;
+  }
+
+  // if the type is when, and params exists, and the first param is an object, destructure the object
+  if (type === "when" && params?.length > 0 && typeof params[0] === "object") {
+    const { comparatorVariable, is, then, otherwise } = params[0];
+    // create an object with the required when params
+    let whenParams = { is: is, then: {} } as whenValidationProps;
+    whenParams.is = is;
+    // add the then object to the whenParams object
+    whenParams.then = (schema: any) => schema[then[0].type](...then[0].params);
+
+    // if there is an otherwise param, add it to the whenParams object
+    if (otherwise) {
+      whenParams.otherwise = (schema: any) =>
+        schema[otherwise[0].type](...otherwise[0].params);
     }
 
-    const { params, type } = validation;
-
-    //check the type is in the yup schema
-    if (!validator[type as keyof typeof validator]) {
-        console.log('returning');
-        return;
-    }
-
-    if (type === 'when' && params.length > 0 && typeof params[0] === 'object') {
-        const { comparatorVariable, is, then, otherwise } = params[0];
-        let whenParams = { is: is, then: {} } as whenValidationProps;
-        whenParams.is = is;
-        whenParams.then = (schema: any) =>
-            schema[then[0].type](...then[0].params);
-
-        if (otherwise) {
-            whenParams.otherwise = (schema: any) =>
-                schema[otherwise[0].type](...otherwise[0].params);
-        }
-
-        return validator['when' as keyof typeof validator](
-            comparatorVariable,
-            whenParams
-        );
-    } else {
-        console.log('params', params);
-        return validator[type as keyof typeof validator](...params);
-    }
+    // return the validator with the when key and the whenParams object
+    return validator["when" as keyof typeof validator](
+      comparatorVariable,
+      whenParams
+    );
+  } else {
+    // if type is not when, return the validator with the type key and the params array
+    return validator[type as keyof typeof validator](...params);
+  }
 };
 
+//this function creates a yup schema from a form schema
 export const yupGeneration = (schema: yupFormBuilderProps) => {
-    const { properties } = schema;
-    const yupSchema = {};
-    try {
-        // const yepSchema = formData.reduce(createYupSchema, {});
-        for (const key in properties) {
-            const { type, validations } = properties[key];
+  //properties in the schema
+  const { properties } = schema;
+  const yupSchema = {};
+  try {
+    //iterate through the properties in the schema
+    for (const key in properties) {
+      //get the type and validations for the current property
+      const { type, validations } = properties[key];
 
-            //check that the key is in the schema
-            if (schema[key as keyof typeof schema]) {
-                console.log('we hit this');
-                continue;
-            }
+      //check that the key is in the schema
+      if (schema[key as keyof typeof schema]) {
+        console.log("we hit this");
+        continue;
+      }
 
-            //create the validator function e.g. yup.string()
-            let validator = (yup[type as keyof typeof yup] as Function)();
+      //create the validator function e.g. yup.string()
+      let validator = (yup[type as keyof typeof yup] as Function)();
 
-            //validations can be a string or an object
-            validations?.forEach((validation) => {
-                let tmpValidator = yupArgCreator(validation, validator);
-                if (tmpValidator) {
-                    validator = tmpValidator;
-                }
-            });
-
-            //as any because typescript doesn't like the dynamic key
-            (yupSchema[key as keyof typeof yupSchema] as any) = validator;
+      //validations can be a string or an object
+      validations?.forEach((validation) => {
+        //create a new validator for the property
+        let tmpValidator = yupArgCreator(validation, validator);
+        if (tmpValidator) {
+          validator = tmpValidator;
         }
-    } catch (e) {
-        console.log('error', e);
+      });
+
+      //as any because typescript doesn't like the dynamic key
+      (yupSchema[key as keyof typeof yupSchema] as any) = validator;
     }
-    return yup.object().shape(yupSchema);
-
+  } catch (e) {
+    console.log("error", e);
+  }
+  //return the schema
+  return yup.object().shape(yupSchema);
 };
-
