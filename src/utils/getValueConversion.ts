5d878bd9f99a16dd79e30ad03f5506c717dd965b
River Hendriksen <RIH27@pitt.edu>
2023-08-15 12:01:40 -0400

    boolean

--- src/utils/getValueConversion.ts
+++ src/utils/getValueConversion.ts
@@ -1,26 +1,34 @@
-import { FieldValues } from 'react-hook-form';
+import { FieldValues } from "react-hook-form";
 
 export const GetValueTyped = <T>(vals: FieldValues): T | null => {
-    return convertFormValues(vals) as T;
+  return convertFormValues(vals) as T;
 };
 
 export function convertFormValues<T extends Record<string, any>>(values: T): T {
-    const convertedValues: Partial<T> = {};
+  const convertedValues: Partial<T> = {};
 
-    for (const key in values) {
-        if (typeof values[key] === 'string') {
-            const numericValue = parseFloat(values[key] as string);
-            if (!isNaN(numericValue)) {
-                convertedValues[key] = numericValue as T[typeof key]; // Type assertion here
-                continue;
-            }
-            if (values[key] === '') {
-                convertedValues[key] = undefined; // Type assertion here
-                continue;
-            }
-        }
-        convertedValues[key] = values[key];
+  for (const key in values) {
+    if (typeof values[key] === "string") {
+      const numericValue = parseFloat(values[key] as string);
+      if (!isNaN(numericValue)) {
+        convertedValues[key] = numericValue as T[typeof key]; // Type assertion here
+        continue;
+      }
+      if (values[key] === "") {
+        convertedValues[key] = undefined; // Type assertion here
+        continue;
+      }
+      if (values[key] === "true") {
+        convertedValues[key] = true as T[typeof key]; // Type assertion here
+        continue;
+      }
+      if (values[key] === "false") {
+        convertedValues[key] = false as T[typeof key]; // Type assertion here
+        continue;
+      }
     }
+    convertedValues[key] = values[key];
+  }
 
-    return convertedValues as T;
+  return convertedValues as T;
 }
