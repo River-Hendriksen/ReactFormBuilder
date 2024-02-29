ab14cc0906dc6789d8647c4ac9683473eb4d0108
River Hendriksen <RIH27@pitt.edu>
2024-02-29 16:45:23 -0500

    minor fixes

--- src/components/yupSchemaCreator/storedProcedures.ts
+++ src/components/yupSchemaCreator/storedProcedures.ts
@@ -10,6 +10,10 @@ const decimalRegex = (numberOfDecimals: string) => {
   return new RegExp("^-?[0-9]*\\.?[0-9]{0," + numberOfDecimals + "}$");
 };
 
+const isNullOrUndefined = (variable: any) => {
+  return variable == "" || variable == null || variable == undefined;
+}; //just in case of a boolean
+
 //switch statement to determine which validation function to use
 export const storedProcedures = (
   validationType: string,
@@ -18,9 +22,13 @@ export const storedProcedures = (
   switch (validationType) {
     case "isNumber":
       return (variable: any) =>
+        isNullOrUndefined(variable) ||
         new RegExp(/^-?[0-9]\d*(\.\d+)?$/).test(variable);
     case "isPhoneNumber":
       return (variable: any) =>
+        variable == "" ||
+        variable == null ||
+        variable == undefined ||
         new RegExp(
           /^(\+?\d{0,4})?\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{4}\)?)?$/
         ).test(variable);
@@ -42,6 +50,18 @@ export const storedProcedures = (
         try {
           return (
             expectedValue == Number(functionArguements["whenOptionIdArgIs"])
+          );
+        } catch {
+          throw new Error(
+            "whenOptionIdIs: whenOptionIdArgIs is not a number or does not exist"
+          );
+        }
+      };
+    case "whenOptionIdIsNot":
+      return (expectedValue: number) => {
+        try {
+          return (
+            expectedValue !== Number(functionArguements["whenOptionIdArgIs"])
           );
         } catch {
           throw new Error(
